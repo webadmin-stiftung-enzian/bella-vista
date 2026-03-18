113e43e3ddda1f36d7c8118dee3bded9f8c8315a
Jörg Adomaitis <joerg.adomaitis@stiftung-enzian.ch>
2026-03-18 16:40:11 +0100

    update smooth scroll offset for header height

--- app/public/wp-content/themes/enzian/assets/scripts/smooth-scroll.js
+++ app/public/wp-content/themes/enzian/assets/scripts/smooth-scroll.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
           duration: 1.5,
           scrollTo: {
             y: targetElement,
-            offsetY: 150,
+            offsetY: 175,
             autoKill: false
           },
           ease: "power2.inOut",
