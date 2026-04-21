a24f81870e6461ebaec249641536017cbed7530d
Jörg Adomaitis <joerg.adomaitis@stiftung-enzian.ch>
2026-04-21 16:29:38 +0200

    change font preload

--- app/public/wp-content/themes/enzian/functions.php
+++ app/public/wp-content/themes/enzian/functions.php
@@ -6,8 +6,9 @@ add_filter('wp_preload_resources', function ($preload_resources) {
 
     $fonts = [
         'PPFragment-GlareRegular.woff2',
-        'PPNeueMontreal-Regular.woff2',
-        'PPNeueMontreal-ThinItalic.woff2',
+        'PPFragment-GlareLight.woff2',
+        'PPFragment-GlareLightItalic.woff2',
+        'PPNeueMontreal-Regular.woff2'
     ];
 
     foreach ($fonts as $font) {
